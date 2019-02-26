e85eb5bc51676f2f5ee84fcc1c8dedb5dc0aa669
DthFish <dthfish@163.com>
2019-02-26 21:38:06 +0800

    print tree

---
+++ src/tree/ListTreeByLayer.java
@@ -0,0 +1,65 @@
+package tree;
+
+import java.util.ArrayDeque;
+
+public class ListTreeByLayer {
+
+    public static void main(String[] args) {
+        int count = 10;
+        TreeNode[] arr = new TreeNode[count + 1];
+        for (int i = 1; i <= count; i++) {
+            TreeNode treeNode = new TreeNode(i);
+            arr[i] = treeNode;
+        }
+
+        for (int i = 1; i < count; i++) {
+            if (i * 2 <= count) {
+                arr[i].left = arr[i * 2];
+            }
+
+            if (i * 2 + 1 <= count) {
+                arr[i].right = arr[i * 2 + 1];
+            }
+        }
+        TreeNode root = arr[1];
+        layerOrder(root);
+
+    }
+
+    public static void preOrder(TreeNode root) {
+        if (root == null) return;
+        System.out.println(root.value);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void inOrder(TreeNode root) {
+        if (root == null) return;
+        inOrder(root.left);
+        System.out.println(root.value);
+        inOrder(root.right);
+    }
+
+    public static void postOrder(TreeNode root) {
+        if (root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.value);
+    }
+
+    public static void layerOrder(TreeNode root) {
+        if (root == null) return;
+        ArrayDeque<TreeNode> list = new ArrayDeque<>();
+        list.offer(root);
+        while (!list.isEmpty()) {
+            TreeNode poll = list.poll();
+            System.out.println(poll.value);
+            if (poll.left != null) {
+                list.offer(poll.left);
+            }
+            if (poll.right != null) {
+                list.offer(poll.right);
+            }
+        }
+    }
+}
