56b4064244d52d38531f6a5307762bd5533a3d2f
DthFish <dthfish@163.com>
2019-03-19 20:06:03 +0800

    double list fast sort

--- src/list/DoubleNode.java
+++ src/list/DoubleNode.java
@@ -26,6 +26,7 @@ public class DoubleNode {
         //3,1,4,7,5,8,9,6
         final DoubleNode three = new DoubleNode(3);
         final DoubleNode one = new DoubleNode(1);
+        final DoubleNode two = new DoubleNode(2);
         final DoubleNode four = new DoubleNode(4);
         final DoubleNode seven = new DoubleNode(7);
         final DoubleNode five = new DoubleNode(5);
@@ -34,8 +35,10 @@ public class DoubleNode {
         final DoubleNode six = new DoubleNode(6);
         three.next = one;
         one.pre = three;
-        one.next = four;
-        four.pre = one;
+        one.next = two;
+        two.pre = one;
+        two.next = four;
+        four.pre = two;
         four.next = seven;
         seven.pre = four;
         seven.next = five;
@@ -48,26 +51,27 @@ public class DoubleNode {
         six.pre = nine;
 
         DoubleNode head = three;
-//        while (head != null) {
-//            System.out.println(head.value);
-//            head = head.next;
-//        }
         DoubleNode tail = six;
-//        while (tail != null) {
-//            System.out.println(tail.value);
-//            tail = tail.pre;
-//        }
         //--------------上面准备的数据
         quickSort(head, tail);
-        while (head != null) {
-            System.out.println(head.value);
-            head = head.next;
-        }
+        printList(head);
 
     }
 
+    private static void printList(DoubleNode head) {
+        while (head != null) {
+            if (head.next == null) {
+                System.out.println(head.value);
+            } else {
+                System.out.print(head.value);
+            }
+            head = head.next;
+        }
+    }
+
     private static void quickSort(DoubleNode head, DoubleNode tail) {
-        if (head == tail || head == null || tail == null) {
+        //特别注意：链表操作 pre next 会超过原来的 head 或者 tail
+        if (head == tail || head == null || tail == null || head.pre == tail || tail.next == head) {
             return;
         }
 
@@ -83,91 +87,22 @@ public class DoubleNode {
         DoubleNode index = head;
         while (index != null && index != tail) {
             if (index.value < tail.value) {
-                if (point != index) {
-                    DoubleNode temp = new DoubleNode(-1);
-
-                    temp.next = point.next;
-                    point.next = temp;
-                    temp.pre = point;
-                    if (temp.next != null) {
-                        temp.next.pre = temp;
-                    }
-
-                    DoubleNode pre = point.pre;
-                    DoubleNode next = point.next;
-
-                    DoubleNode indexPre = index.pre;
-                    DoubleNode indexNext = index.next;
-
-                    if (pre != null)
-                        pre.next = index;
-                    index.pre = pre;
-                    index.next = next;
-                    next.pre = index;
-
-                    if (indexPre != null)
-                        indexPre.next = point;
-                    point.pre = indexPre;
-                    point.next = indexNext;
-                    indexNext.pre = point;
-
-                    index.next = index.next.next;
-                    if (index.next != null) {
-                        index.next.pre = index;
-                    }
-
-
-                    temp = point;
-                    point = index;
-                    index = temp;
+                if (index != point) {
+                    int temp = point.value;
+                    point.value = index.value;
+                    index.value = temp;
                 }
-
                 point = point.next;
-
             }
-
             index = index.next;
         }
         if (point != tail) {
-            DoubleNode temp = new DoubleNode(-1);
-
-            temp.next = point.next;
-            point.next = temp;
-            temp.pre = point;
-            if (temp.next != null) {
-                temp.next.pre = temp;
-            }
-
-
-            DoubleNode pre = point.pre;
-            DoubleNode next = point.next;
-
-            DoubleNode tailPre = tail.pre;
-            DoubleNode tailNext = tail.next;
-
-            if (pre != null)
-                pre.next = tail;
-            tail.pre = pre;
-            tail.next = next;
-            next.pre = tail;
-            if (tailPre != null)
-                tailPre.next = point;
-            point.pre = tailPre;
-            point.next = tailNext;
-            if (tailNext != null)
-                tailNext.pre = point;
-
-            tail.next = tail.next.next;
-            if (tail.next != null) {
-                tail.next.pre = tail;
-            }
-
-            temp = point;
-            point = tail;
-            tail = temp;
+            int temp = point.value;
+            point.value = tail.value;
+            tail.value = temp;
         }
-        System.out.println(point.value);
         return point;
     }
-
 }
+
+
