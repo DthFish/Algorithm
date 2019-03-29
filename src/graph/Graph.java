74df2bf72644ac70ef5090b481eb6a67ab0ba110
DthFish <dthfish@163.com>
2019-03-29 10:07:24 +0800

    拓扑排序

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,114 +1,65 @@
 package graph;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
-@SuppressWarnings("unchecked")
+/**
+ * 拓扑排序：
+ */
 public class Graph {
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(6);
+        graph.addEdge(0,4);
+        graph.addEdge(4,1);
+        graph.addEdge(1,5);
+        graph.addEdge(2,3);
+        graph.addEdge(3,5);
+        graph.topoSortByKahn();
+    }
     private int v;
     private LinkedList<Integer>[] adj;
 
     public Graph(int v) {
         this.v = v;
-        this.adj = new LinkedList[v];
+        adj = new LinkedList[v];
         for (int i = 0; i < v; i++) {
-            this.adj[i] = new LinkedList<>();
+            adj[i] = new LinkedList<>();
         }
     }
 
     public void addEdge(int s, int t) {
         adj[s].add(t);
-        adj[t].add(s);
-    }
-
-    public static void main(String[] args) {
-        Graph graph = new Graph(8);
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 3);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 4);
-        graph.addEdge(3, 4);
-        graph.addEdge(2, 5);
-        graph.addEdge(4, 5);
-        graph.addEdge(4, 6);
-        graph.addEdge(5, 7);
-        graph.addEdge(6, 7);
-
-        graph.bfs(0, 6);
-        System.out.println("-----------");
-        graph.dfs(0, 6);
-
     }
 
     /**
-     * Breadth-First-Search
+     * 1. 统计各个节点的入度
      */
-    public void bfs(int s, int t) {
-        if (s == t) return;
-        int[] prev = new int[v];
+    public void topoSortByKahn() {
+        int[] inDegree = new int[v];
         for (int i = 0; i < v; i++) {
-            prev[i] = -1;
-        }
-        boolean[] visited = new boolean[v];
-        visited[s] = true;
-        LinkedList<Integer> queue = new LinkedList<>();
-        queue.add(s);
-        while (!queue.isEmpty()) {
-            @SuppressWarnings("ConstantConditions")
-            int w = queue.poll();
-
-            for (int i = 0; i < adj[w].size(); i++) {
-                int q = adj[w].get(i);
-                if (!visited[q]) {
-                    prev[q] = w;
-                    if (q == t) {
-                        print(prev, s, t);
-                        return;
-                    }
-                    visited[q] = true;
-                    queue.add(q);
-                }
+            for (int j = 0; j < adj[i].size(); j++) {
+                inDegree[adj[i].get(j)]++;
             }
-
         }
 
+        Queue<Integer> queue = new LinkedList<>();
 
-    }
-
-    private void print(int[] pre, int s, int t) {
-        if (pre[t] != -1 && s != t) {
-            print(pre, s, pre[t]);
-        }
-        System.out.println(String.valueOf(t));
-    }
-
-    private boolean found = false;
-
-    /**
-     * Depth-First-Search
-     */
-    public void dfs(int s, int t) {
-        found = false;
-        int[] prev = new int[v];
         for (int i = 0; i < v; i++) {
-            prev[i] = -1;
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
         }
-        boolean[] visited = new boolean[v];
-        dfsPart(s, t, prev, visited);
-        print(prev, s, t);
-    }
 
-    private void dfsPart(int s, int t, int[] prev, boolean[] visited) {
-        if (s == t) {
-            found = true;
-        }
-        if (found) return;
-        visited[s] = true;
-        for (int i = 0; i < adj[s].size(); i++) {
-            int q = adj[s].get(i);
-            if (!visited[q]) {
-                prev[q] = s;
-                dfsPart(q, t, prev, visited);
-
+        while (!queue.isEmpty()) {
+            int poll = queue.poll();
+            System.out.println("-->" + poll);
+            for (int i = 0; i < adj[poll].size(); i++) {
+                int get = adj[poll].get(i);
+                inDegree[get]--;
+                if (inDegree[get] == 0) {
+                    queue.offer(get);
+                }
             }
         }
     }
