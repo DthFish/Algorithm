772a2a20c0a1c4fd4f92b4c193e816a670063026
DthFish <dthfish@163.com>
2019-03-29 20:34:16 +0800

    拓扑排序，逆接邻表法

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -10,13 +10,16 @@ public class Graph {
 
     public static void main(String[] args) {
         Graph graph = new Graph(6);
-        graph.addEdge(0,4);
-        graph.addEdge(4,1);
-        graph.addEdge(1,5);
-        graph.addEdge(2,3);
-        graph.addEdge(3,5);
+        graph.addEdge(0, 4);
+        graph.addEdge(4, 1);
+        graph.addEdge(1, 5);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 5);
         graph.topoSortByKahn();
+        System.out.println("-------");
+        graph.topoSortByDFS();
     }
+
     private int v;
     private LinkedList<Integer>[] adj;
 
@@ -64,4 +67,40 @@ public class Graph {
         }
     }
 
+    /**
+     * 借助逆接邻表的深度优先遍历达到拓扑排序的目的
+     */
+    public void topoSortByDFS() {
+        LinkedList<Integer>[] reverseAdj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            reverseAdj[i] = new LinkedList<>();
+        }
+
+        for (int i = 0; i < adj.length; i++) {// 通过接邻表生成逆接邻表
+            for (int j = 0; j < adj[i].size(); j++) {
+                int k = adj[i].get(j);
+                reverseAdj[k].add(i);
+            }
+        }
+
+        boolean[] visited = new boolean[v];
+
+        for (int i = 0; i < v; i++) {// 对于任意一个顶点，先遍历该顶点可达顶点，在遍历自己（因为是逆接邻表，最终表现出来的就是先遍历出无依赖的顶点）
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i, reverseAdj, visited);
+            }
+        }
+
+    }
+
+    private void dfs(int i, LinkedList<Integer>[] reverseAdj, boolean[] visited) {
+        for (int j = 0; j < reverseAdj[i].size(); j++) {
+            int k = reverseAdj[i].get(j);
+            if (visited[k]) continue;
+            visited[k] = true;
+            dfs(k, reverseAdj, visited);
+        }
+        System.out.println("-->" + i);
+    }
 }
