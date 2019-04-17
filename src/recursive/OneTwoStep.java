4d6d9b8c1d3e10406080f7f798e61d11edbe5c0f
DthFish <dthfish@163.com>
2019-04-17 20:35:18 +0800

    one two step

--- src/recursive/OneTwoStep.java
+++ src/recursive/OneTwoStep.java
@@ -11,6 +11,7 @@ public class OneTwoStep {
         System.out.println(step(2));
         System.out.println(step(3));
         System.out.println(step(4));
+        System.out.println(climbStairs(44));
     }
 
     public static int step(int n) {
@@ -20,5 +21,17 @@ public class OneTwoStep {
             return 2;
         }
         return step(n - 1) + step(n - 2);
+    }
+
+    /**
+     * 动态规划思想，效率比{@link #step(int)}更快
+     */
+    public static int climbStairs(int n) {
+        int[] d = new int[n + 1];
+        d[0] = d[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            d[i] = d[i - 1] + d[i - 2];
+        }
+        return d[n];
     }
 }
