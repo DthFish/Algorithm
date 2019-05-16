3bbdfad65f8fecfc1a25b0daf7f08ca7a0dff4dd
DthFish <dthfish@163.com>
2019-05-16 10:26:24 +0800

    leetcode 62 不同路径 减少比较次数

--- src/leetcode/Question62.java
+++ src/leetcode/Question62.java
@@ -10,17 +10,19 @@ public class Question62 {
         System.out.println(uniquePaths(3, 2));
         System.out.println(uniquePaths(7, 3));
     }
+
     // 动态规划，主要是递推公式
     public static int uniquePaths(int m, int n) {
         int[][] status = new int[m][n];
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    status[i][j] = 1;
-                } else {
-                    status[i][j] = status[i - 1][j] + status[i][j - 1];
-                }
-
+            status[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            status[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                status[i][j] = status[i - 1][j] + status[i][j - 1];
             }
         }
         return status[m - 1][n - 1];
